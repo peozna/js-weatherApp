cbf50b5ebdf93ca11cd4f84f1c8df946107bf17e
Nathalie <nannaskrutt@gmail.com>
2026-05-29 10:53:58 +0200

    Added logic for describing weather codes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchInput = document.getElementById('searchInput')
 const searchButton = document.getElementById('searchButton')
 const cityName = document.getElementById('cityName')
 const temperature = document.getElementById('temperature')
+const description = document.getElementById('description')
 const windSpeed = document.getElementById('windSpeed')
 
 //Event listener for search button that reads the input value and prints it to the console
@@ -25,6 +26,34 @@ searchButton.addEventListener('click', (event) => {
             .then(res => res.json())
             .then(data => {
                temperature.textContent = "Temperature: " + data.current_weather.temperature + "°C"
+
+               //The weather code is a number that represents the current weather condition. The following codes are used:
+                description.textContent = data.current_weather.weathercode
+                    if(data.current_weather.weathercode === 0) {
+                        description.textContent = "Clear sky"
+                    }
+                    else if(1 < data.current_weather.weathercode > 3) { 
+                        description.textContent = "Partly cloudy"
+                    }
+                    else if(3 < data.current_weather.weathercode > 48) { 
+                        description.textContent = "Cloudy"
+                    }
+                    else if(48 < data.current_weather.weathercode > 57) {
+                        description.textContent = "Fog"
+                    }
+                    else if(57 < data.current_weather.weathercode > 67) {
+                        description.textContent = "Drizzle"
+                    }
+                    else if(67 < data.current_weather.weathercode > 77) {
+                        description.textContent = "Rain"
+                    }
+                    else if(77 < data.current_weather.weathercode > 86) {
+                        description.textContent = "Snow"
+                    }
+                    else if(86 < data.current_weather.weathercode > 95) {
+                        description.textContent = "Thunderstorm"
+                    }
+
                windSpeed.textContent = "Wind Speed: " + data.current_weather.windspeed + " km/h"
                cityName.textContent = city
           })
